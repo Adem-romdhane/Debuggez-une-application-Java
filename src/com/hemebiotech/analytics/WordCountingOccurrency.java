f4f9f60cfd5edc13536bb10635d8e97776c35cc1
Adam Romdhane <ademromdhane@yahoo.com>
2022-12-06 19:24:17 +0100

    ajout ddu comptage des occurrences par symptomes

--- src/com/hemebiotech/analytics/WordCountingOccurrency.java
+++ src/com/hemebiotech/analytics/WordCountingOccurrency.java
@@ -2,27 +2,29 @@ package com.hemebiotech.analytics;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-public class WordCounting {
+public class WordCountingOccurrency {
     TreeMap<String,Integer> map=null;
     private String filename;
 
-    public WordCounting(String filename){
+    public WordCountingOccurrency(String filename) {
         this.filename = filename;
         map = new TreeMap<String, Integer>();
     }
 
-    public TreeMap<String,Integer> getWordCount() throws FileNotFoundException {
+
+
+    public TreeMap<String,Integer> getWordCountingOccurrency() throws FileNotFoundException {
         if (map.isEmpty()){
             parseWord();
 
         }
-
     return map;
     }
+
+
 
     private void parseWord() throws FileNotFoundException {
         Scanner txtFile = new Scanner(new File(filename));
--- src/com/hemebiotech/analytics/main.java
+++ src/com/hemebiotech/analytics/main.java
@@ -1,41 +1,29 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class main {
     public static void main(String args[]) throws IOException {
 
 
-        new SortList().SortList("symptoms.txt","symptoms.txt");
-        AnalyticsCounter analyticsCounter= new AnalyticsCounter();
+        new SortList().SortList("symptoms.txt", "symptoms.txt");
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
         analyticsCounter.ReadFile();
 
 
+        new SortList().SortList("results.out", "results.out");
 
-
-        new SortList().SortList("results.out","results.out");
-
-        WordCounting wordCounting = new WordCounting("symptoms.txt");
-
-        TreeMap<String,Integer> map = wordCounting.getWordCount();
-
-        for (Map.Entry<String, Integer> entry : map.entrySet()){
+        WordCountingOccurrency wordCounting = new WordCountingOccurrency("symptoms.txt");
+        TreeMap<String, Integer> map = wordCounting.getWordCountingOccurrency();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.println(entry);
 
 
-
         }
-
-
-
-
-
+        FileWriter writer = new FileWriter("results.out");
+        writer.write(String.valueOf(map)+"\n");
+        writer.close();
     }
 }
